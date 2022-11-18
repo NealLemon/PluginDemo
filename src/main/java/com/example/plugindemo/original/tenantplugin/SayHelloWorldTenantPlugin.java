107af4b7b42bc9bf404f5d7f0c22ca3571826997
za-zhushaobin <shaobin.zhu@zatech.com>
2022-11-18 16:28:35 +0800

    update

--- src/main/java/com/example/plugindemo/original/tenantplugin/SayHelloWorldTenantPlugin.java
+++ src/main/java/com/example/plugindemo/original/tenantplugin/SayHelloWorldTenantPlugin.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
  * @Description:
  * @Version 1.0.0
  */
-//@Service
+@Service
 public class SayHelloWorldTenantPlugin implements SayHelloWorld {
     @Override
     public String sayHelloWorld() {
--- src/main/java/com/example/plugindemo/test/SayHelloController.java
+++ src/main/java/com/example/plugindemo/test/SayHelloController.java
@@ -1,4 +1,4 @@
-package com.example.plugindemo.base;
+package com.example.plugindemo.test;
 
 import com.example.plugindemo.base.sample.SayHelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
