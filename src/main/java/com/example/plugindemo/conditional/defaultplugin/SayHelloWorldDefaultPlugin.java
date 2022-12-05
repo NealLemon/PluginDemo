022775c9f930ba5a856442ca72fde5a36e4dabe2
NealLemon <asas20@126.com>
2022-12-05 22:00:43 +0800

    update

--- src/main/java/com/example/plugindemo/conditional/defaultplugin/SayHelloWorldDefaultPlugin.java
+++ src/main/java/com/example/plugindemo/conditional/defaultplugin/SayHelloWorldDefaultPlugin.java
@@ -1,6 +1,7 @@
 package com.example.plugindemo.conditional.defaultplugin;
 
 import com.example.plugindemo.base.SayWorld;
+import com.example.plugindemo.conditional.PluginCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
  **/
 //判断是否有该 插件类别的Bean 已经注册在容器中,如果没有 则注册 即注册默认的插件
 @ConditionalOnMissingBean(type = {"SayWorld"})
+//自定义条件装配注解
+@PluginCondition()
 @Service
 public class SayHelloWorldDefaultPlugin implements SayWorld {
     @Override
